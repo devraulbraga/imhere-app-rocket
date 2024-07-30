2d564d07b0d1ad9f92d6acb1cb3ae241b0e9a646
Raul Lopes <raulbraga_@hotmail.com>
2024-07-30 09:31:02 -0400

    Functions

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,67 @@
-import { Text, View, TextInput, TouchableOpacity} from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from "react-native";
 import { Participant } from "../../components/participant";
 import { styles } from "./style";
+import { useState } from "react";
 
 export default function Home() {
+  const [participant, setParticipant] = useState("");
+  const participants = ["Raul", "Rafael", "Ronaldo"];
+
+  function handleAddParticipant() {
+    if (participants.includes(participant)) {
+      Alert.alert("Aviso", "Participante já cadastrado!");
+      setParticipant("");
+      return;
+    }
+  }
+  function handleParticipatRemove(name: string) {
+    Alert.alert("Aviso", `Deseja remover ${name} do evento?`, [
+      { text: "Não", style: "cancel" },
+      { text: "Sim" },
+    ]);
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-        <View style={styles.form}>
-        <TextInput style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6b6b6b"
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+          onChangeText={setParticipant}
+          value={participant}
         />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
-      <Participant />
+      <FlatList
+        data={participants} // lista de participants
+        keyExtractor={(item) => item} // extrato de participants
+        renderItem={({ item }) => (
+          <Participant
+            name={item}
+            onRemove={() => handleParticipatRemove(item)}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text
+            style={{ color: "#6b6b6b", textAlign: "center", marginTop: 20 }}
+          >
+            Nenhum participante cadastrado
+          </Text>
+        )}
+      />
     </View>
-  )
+  );
 }
